b922461da842ab0675bb6cce3b6eacc8b8c90f83
unknown <kuldeepgangarekar2001@gmail.com>
2024-12-22 21:59:32 +0530

    Book instance Array

---
+++ books.js
@@ -0,0 +1,11 @@
+  
+ import {Book} from './Book.js';
+ 
+ //Creating an array of Book instances
+ const books = [
+   new Book('The hero','J J Collin',1998),
+   new Book('The great Bridge','Ben Cutting',1966)
+   ]
+   
+ //export this books
+ export {books};
